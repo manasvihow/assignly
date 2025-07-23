8370a034b7c1ed88a4061432d8ec95f9f1f9fd61
Manasvi Bathula <164844856+manasvihow@users.noreply.github.com>
2025-07-23 14:19:10 +0530

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,6 +3,8 @@ from ..core.config import settings
 
 connect_args = {"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
 
+engine = create_engine(settings.DATABASE_URL, echo=True, connect_args=connect_args)
+
 def get_session():
     with Session(engine) as session:
         yield session
