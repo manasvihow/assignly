234e82940bdf206894b8b99d98687d6574ee8c75
Manasvi Bathula <164844856+manasvihow@users.noreply.github.com>
2025-07-23 14:55:41 +0530

    Update submissions.py

--- backend/api/v1/endpoints/submissions.py
+++ backend/api/v1/endpoints/submissions.py
@@ -1,10 +1,10 @@
 from typing import List
 from fastapi import APIRouter, Depends
-from sqlmodel import Session, select
+from sqlmodel import Session
 
 from ....db.database import get_session
-from ....db.models import Submission, User
 from ....core.security import get_current_student
+from ....db.models import User
 from ....api.v1.schemas.assignment_schemas import SubmissionRead
 
 router = APIRouter()
@@ -15,7 +15,4 @@ def get_my_submissions(
     session: Session = Depends(get_session),
     current_user: User = Depends(get_current_student),
 ):
-    """
-    Get all submissions for the currently logged-in student.
-    """
     return current_user.submissions
